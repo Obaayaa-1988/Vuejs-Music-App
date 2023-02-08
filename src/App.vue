239ba2e732e951aeb466e524d7dae41dd3657ce2
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-02-08 21:42:35 +0000

    click event for playlist

--- src/App.vue
+++ src/App.vue
@@ -19,7 +19,9 @@
 
       <section class="playlist">
        <h3> The Playlist</h3> 
-       <button v-for="song in songs" :key="song.src">
+       <button v-for="song in songs" :key="song.src" @click="play(song)" :class="(song.src === 
+        current.src) ? 'song playing' :'song'">
+        {{ song.title - }} {{ song.artist }}
       
       </button>
 
