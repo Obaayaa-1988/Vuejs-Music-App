ba89939b2eaa7016d0c5593995baaccc15188860
Obaayaa-1988 <aquaicoe31@gmail.com>
2023-02-08 21:26:07 +0000

    added else if for pause method

--- src/App.vue
+++ src/App.vue
@@ -10,11 +10,15 @@
         <div class="control"> 
           <button class="prev">Prev</button>
           <button class="play" v-if="!isPlaying">Play</button>
-          <button class="pause" v-else>Pause</button>
+          <button class="pause" v-else @click="pause">Pause</button>
           <button class="next">Next</button>
 
         </div>
 
+      </section>
+
+      <section> 
+        
       </section>
 
     </main>
